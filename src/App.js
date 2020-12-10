761011fbe904b8c24525d431b0ab23b28e055863
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-12-10 10:25:45 +0100

    Define styling and create basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <h1>TO DO LIST_</h1>
+      <button class="btn">
+        <span class="btn__content">Create new task_</span>
+        <span class="btn__glitch"></span>
+        <span class="btn__label">r25</span>
+      </button>
     </div>
   );
 }
